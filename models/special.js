407805fc549ab7859df7f0755ea2f09b947a6493
Kent <kent@Kents-MacBook-Pro.local>
2018-08-03 14:23:20 -0700

    add models

---
+++ models/special.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var specialSchema = new Schema ({
+    content: String,
+    hot: {type: Boolean, default: false}, 
+    votes: {upvotes: Number, downvotes: Number},
+    category: {type: String, enum: ['Food', 'Drink', 'Food & Drink', 'Other']},
+    spot: {type: Schema.Types.ObjectId, ref: 'Spot'},
+    verified: {type: Boolean, default: false},
+    when: { hours: String, days: String}
+});
+
+module.exports = mongoose.model('Special', specialSchema);
