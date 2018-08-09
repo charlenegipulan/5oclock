650c96cc3370b19cea69ad1108c83308a46e7e33
Ivy Lim <yunjilim@gmail.com>
2018-08-09 12:10:49 -0700

    disabled up and down votes

--- public/javascripts/rt-votes.js
+++ public/javascripts/rt-votes.js
@@ -1,12 +1,13 @@
 var socket = io();
 
 socket.on('vote-changed', function(specialDoc) {
-    console.log(JSON.stringify(specialDoc));
     var upvoteDiv = document.querySelector(`#special-${specialDoc._id} div.upvote`);
     if (upvoteDiv) {
         var downvoteDiv = document.querySelector(`#special-${specialDoc._id} div.downvote`);
         upvoteDiv.innerHTML = specialDoc.votes.upvotes;
         downvoteDiv.innerHTML = specialDoc.votes.downvotes;
+        document.getElementById("upBtn").disabled = true;
+        document.getElementById("downBtn").disabled = true;
     }
 });
 
